dbf483d71af91da8ddf0c1c5ddfa67d0ebf2da10
Jolan <jolan.brizard@epitech.eu>
2024-03-05 10:07:04 +0100

    add lint scripts

--- apps/front/nuxt.config.ts
+++ apps/front/nuxt.config.ts
@@ -1,9 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  
+
+  modules: ["@nuxtjs/eslint-module"],
+
   typescript: {
     strict: true,
     shim: false,
   },
-})
+});
