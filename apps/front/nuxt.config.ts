5196fbbf181cf2a5c792b564a8d0a1f04acb9663
Jolan <jolan.brizard@epitech.eu>
2024-03-12 09:50:26 +0100

    feat: setup ui (#1)
    * feat: add nuxtui and tailwind conf
    
    * feat: change gray color and add demo page
    
    * fix: lint

--- apps/front/nuxt.config.ts
+++ apps/front/nuxt.config.ts
@@ -2,6 +2,36 @@
 export default defineNuxtConfig({
   devtools: { enabled: true },
 
+  app: {
+    head: {
+      title: "Lana",
+    },
+    pageTransition: {
+      name: "fade",
+      mode: "out-in",
+    },
+  },
+
+  colorMode: {
+    preference: "light",
+  },
+
+  css: ["~/assets/main.css"],
+
+  modules: [
+    "@nuxt/ui",
+    "@vueuse/nuxt",
+    [
+      "@nuxtjs/google-fonts",
+      {
+        families: {
+          Roboto: true,
+          Raleway: true,
+        },
+      },
+    ],
+  ],
+
   typescript: {
     strict: true,
     shim: false,
---
+++ apps/front/pages/demo.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts"></script>
+
+<template>
+  <UCard class="mt-2 ml-2 w-1/3">
+    <template #default>
+      <span>Hi i'm sam</span>
+    </template>
+    <template #footer>
+      <UButton to="/demo" size="xl"> Click to test </UButton>
+    </template>
+  </UCard>
+</template>
+
+<style scoped></style>
