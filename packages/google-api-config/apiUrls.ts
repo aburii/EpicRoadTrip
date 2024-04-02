c9a67163722a35c9221a58bb887f1458f541e4fe
Jolan <jolan.brizard@epitech.eu>
2024-04-02 09:49:52 +0200

    Feature/form autocomplete (#7)
    * feat: autocomplete endpoint
    
    * feat: autocomplete input menu
    
    upgrade nuxtui

--- packages/google-api-config/apiUrls.ts
+++ packages/google-api-config/apiUrls.ts
@@ -2,3 +2,6 @@ export const directionsUrl: string =
   "https://maps.googleapis.com/maps/api/directions/json";
 export const placesUrl: string =
   "https://maps.googleapis.com/maps/api/place/nearbysearch/json";
+
+export const autocompleteUrl =
+  "https://maps.googleapis.com/maps/api/place/autocomplete/json";
