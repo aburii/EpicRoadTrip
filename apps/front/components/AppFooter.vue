d2fd35d9f96a3c88289ceac4b0d187956662002e
Jolan <jolan.brizard@gmail.com>
2024-04-15 17:13:11 +0200

    feat: legal mentions (to translate)

--- apps/front/components/AppFooter.vue
+++ apps/front/components/AppFooter.vue
@@ -1,14 +1,21 @@
 <script setup lang="ts">
 const { t } = useI18n();
+const localePath = useLocalePath();
+
+const currentYear = new Date().getFullYear();
 </script>
 
 <template>
   <footer class="min-h-[80px] md:px-16 px-4 py-2 bg-transparent">
     <section class="w-full h-full flex flex-col-reverse md:flex-row items-center justify-between">
-      <div class="text-xs">© 2024 Lana. {{ t('footer.copyrights') }}</div>
+      <div class="text-xs">© {{ currentYear }} Lana. {{ t('footer.copyrights') }}</div>
       <div>
-        <UButton :label="t('footer.termsOfUse')" color="black" variant="link" />
-        <UButton :label="t('footer.privacy')" color="black" variant="link" />
+        <UButton
+          :label="t('footer.legalMentions')"
+          color="black"
+          variant="link"
+          :to="localePath('/legal-mentions')"
+        />
       </div>
     </section>
   </footer>
