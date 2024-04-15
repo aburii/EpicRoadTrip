cde37066d83ffbc683e18a063338e35b1be2d7d0
kieran <kieran.boudouin@epitech.eu>
2024-04-15 15:16:12 +0200

    **hotfix auth midleware

--- apps/front/middleware/auth.ts
+++ apps/front/middleware/auth.ts
@@ -5,6 +5,6 @@ export default defineNuxtRouteMiddleware(async () => {
   const session = await $supabase.auth.getSession();
 
   if (!session.data.session) {
-    return navigateTo(localePath('/Demologin'));
+    return navigateTo(localePath('/'));
   }
 });
